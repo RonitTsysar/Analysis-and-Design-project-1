6c123c9fb362cec614c6a055ce2d2d0319f98480
Roy Dor <roydor8@gmail.com>
2020-10-31 16:52:34 +0200

    PrimiumAccount, Product, Supplier - Done (include Connections)

--- PremiumAccount.java
+++ PremiumAccount.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PremiumAccount extends Account {
 
+    private List<Product> products;
+
+    public PremiumAccount(String id) {
+        super(id);
+        this.products = new ArrayList<>();
+    }
 }
--- Product.java
+++ Product.java
@@ -1,4 +1,15 @@
+import java.util.function.Supplier;
+
 public class Product {
     private String id;
     private String name;
+
+    private Supplier supplier;
+
+    public Product(String id, String name, Supplier supplier) {
+        this.id = id;
+        this.name = name;
+
+        this.supplier = supplier;
+    }
 }
